28063e260bc8e8bfdcca760027674583e15357cc
nikandri9 <veronika.seroshtan@gmail.com>
2021-04-03 15:54:38 +0800

    added stuff

---
+++ pages/menu/menu.js
@@ -0,0 +1,41 @@
+// pages/menu/menu.js
+Page({
+
+  data: {
+
+  },
+
+  onLoad: function (options) {
+
+  },
+
+  onReady: function () {
+
+  },
+
+  onShow: function () {
+
+  },
+
+
+  onHide: function () {
+
+  },
+
+  onUnload: function () {
+
+  },
+
+  onPullDownRefresh: function () {
+
+  },
+
+
+  onReachBottom: function () {
+
+  },
+
+  onShareAppMessage: function () {
+
+  }
+})
