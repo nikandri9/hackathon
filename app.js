e8c02fa5140dd32a91dca1966b9122d07b8f5294
Danjie Hu <danjiehu@outlook.com>
2021-04-03 14:37:08 +0800

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+let config = require('./key')
+
+App({
+  onLaunch() {
+
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
+     // 应用名称: {{app_name}}
+    wx.BaaS.init(config.appKey)
+    
+
+    
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ key.js
@@ -0,0 +1,3 @@
+module.exports = {
+  appKey: '4095e1de147e136c208a'
+}
