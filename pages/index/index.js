4cc32d56b58e502dd6a6eac9ab1fe00b1c70a5f1
Danjie Hu <danjiehu@outlook.com>
2021-04-03 17:06:59 +0800

    added feature able to  import diner profile image from iFanr

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,8 +2,30 @@
 // 获取应用实例
 const app = getApp()
 
+//start of page
 Page({
 
-  
+  // start of data
+  data: {
+    dinerProfile: {},
+    // userInfo: userInfo: wx.getStorageSync('userInfo')
+  },
+  // end of data
 
+
+  onLoad: function (res) {
+    console.log("onload",res)
+    let page = this
+    let restaurants = new wx.BaaS.TableObject("restaurant_hack")
+    restaurants.find().then(
+      (res) => {
+        console.log("res_hack_find",res)
+        page.setData({
+          dinerProfile: res.data.objects[0]
+        })
+      })
+    }
+
+
+// end of page
 })
